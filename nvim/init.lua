2900a1f43ef51c7ca0afb6356b9b469b26f633f4
Florian Vallée <florian@vallee.xyz>
2021-06-24 00:15:19 +0200

    nvim: initial init.lua/0.5.0 revamp

---
+++ nvim/init.lua
@@ -0,0 +1,61 @@
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local opt = vim.opt  -- to set options
+
+require 'paq-nvim' {
+	'savq/paq-nvim';
+	'neovim/nvim-lspconfig';
+	'hrsh7th/nvim-compe';
+    'nvim-treesitter/nvim-treesitter';
+
+    -- telescope and dependencies
+    'nvim-lua/popup.nvim';
+    'nvim-lua/plenary.nvim';
+    'nvim-telescope/telescope.nvim';
+
+    -- statusline
+    'itchyny/lightline.vim';
+
+    -- git
+    'nvim-lua/plenary.nvim';
+    'lewis6991/gitsigns.nvim';
+}
+
+require 'compe'.setup({
+	enabled = true;
+	autocomplete = true;
+	documentation = true;
+	source = {
+		path = true;
+		buffer = true;
+		nvim_lsp = true;
+        nvim_lua = true;
+	};
+})
+
+require 'lspconfig'.rust_analyzer.setup {}
+require 'lspconfig'.pyright.setup {}
+require 'nvim-treesitter.configs'.setup{
+    ensure_installed = "maintained";
+    highlight = {enable = true}
+}
+
+vim.cmd[[ highlight GitSignsAdd    guifg=green ]]
+vim.cmd[[ highlight GitSignsChange guifg=orange ]]
+vim.cmd[[ highlight GitSignsDelete guifg=red ]]
+vim.cmd[[ highlight SignColumn guibg=none ]]
+
+require('gitsigns').setup()
+
+vim.o.completeopt = "menuone,noselect"
+
+--vim.cmd 'colorscheme koehler' 
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.textwidth = 79
+opt.smartindent = true
+opt.termguicolors = true
+
+g.mapleader = ','
---
+++ nvim/remap.vim
@@ -0,0 +1,13 @@
+"comp-e
+
+inoremap <silent><expr> <C-Space> compe#complete()
+inoremap <silent><expr> <CR>      compe#confirm('<CR>')
+inoremap <silent><expr> <C-e>     compe#close('<C-e>')
+inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })
+inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })
+
+"telescope
+nnoremap <leader>ff <cmd>Telescope find_files<cr>
+nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+nnoremap <leader>fb <cmd>Telescope buffers<cr>
+nnoremap <leader>fh <cmd>Telescope help_tags<cr>
