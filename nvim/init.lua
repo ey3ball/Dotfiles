037d9b01ac2d8975e98e311f0d4499ec6ed81bee
Florian Vallée <fvallee@fieldbox.ai>
2021-07-19 22:29:05 +0200

    nvim: fix tabs + lspsaga

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,10 @@ local g = vim.g      -- a table to access global variables
 local opt = vim.opt  -- to set options
 
 require 'paq-nvim' {
-	'savq/paq-nvim';
-	'neovim/nvim-lspconfig';
-	'hrsh7th/nvim-compe';
+    'savq/paq-nvim';
+    'neovim/nvim-lspconfig';
+    'glepnir/lspsaga.nvim';
+    'hrsh7th/nvim-compe';
     'nvim-treesitter/nvim-treesitter';
 
     -- telescope and dependencies
@@ -20,26 +21,33 @@ require 'paq-nvim' {
     -- git
     'nvim-lua/plenary.nvim';
     'lewis6991/gitsigns.nvim';
+    'tpope/vim-fugitive';
 }
 
 require 'compe'.setup({
-	enabled = true;
-	autocomplete = true;
-	documentation = true;
-	source = {
-		path = true;
-		buffer = true;
-		nvim_lsp = true;
+    enabled = true;
+    autocomplete = true;
+    documentation = true;
+    source = {
+        path = true;
+        buffer = true;
+        nvim_lsp = true;
         nvim_lua = true;
-	};
+    };
 })
 
 require 'lspconfig'.rust_analyzer.setup {}
 require 'lspconfig'.pyright.setup {}
+require 'lspconfig'.yamlls.setup {
+    filetypes = {"yaml", "yml"};
+}
 require 'nvim-treesitter.configs'.setup{
     ensure_installed = "maintained";
     highlight = {enable = true}
 }
+
+local saga = require 'lspsaga'
+saga.init_lsp_saga()
 
 vim.cmd[[ highlight GitSignsAdd    guifg=green ]]
 vim.cmd[[ highlight GitSignsChange guifg=orange ]]
@@ -59,3 +67,7 @@ opt.smartindent = true
 opt.termguicolors = true
 
 g.mapleader = ','
+g.lightline = {
+    active = { left = { { 'mode', 'paste' }, { 'gitbranch', 'readonly', 'relativepath', 'modified' } } },
+    component_function = { gitbranch = "FugitiveHead" },
+}
